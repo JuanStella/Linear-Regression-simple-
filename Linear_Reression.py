338d110d8d813c9433772409b666610eb1a455e6
Juan Stella <juanstella96@gmail.com>
2024-07-07 17:02:36 -0300

    Plot updates

--- Linear_Reression.py
+++ Linear_Reression.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 class LinearRegression:
 
@@ -13,6 +14,9 @@ class LinearRegression:
         self.y = np.zeros(self.datalen-1)
         self.m = 0
         self.b = 0
+        self.m_history = []
+        self.b_history = []
+        self.j_history = []
 
     def load_data(self):
         with open('D:\ML\MyProgress\Linear Regression\First-Linear-Regression\Salary Dataset (Linear Regression)\\Salary_dataset.csv', 'r') as file:
@@ -30,15 +34,22 @@ class LinearRegression:
                     self.years.append(year)
                     self.prices.append(price)
 
+    def compute_cost(self, m, b):
+        predictions = m * np.array(self.years) + b
+        return 1 / (2 * self.datalen) * np.sum((predictions - self.prices) ** 2)
+
     def fit(self):
         self.y = self.m * np.array(self.years) + self.b  # Calcular las predicciones
 
         it = 0
         while it < self.iter_max:
-            error = 1 / (2 * self.datalen) * np.sum((self.y - self.prices) ** 2)
+            error = self.compute_cost(self.m, self.b)
             if error < 0.0001:
                 break
             # Actualizar m y b utilizando el gradiente descendente
+            self.j_history.append(error)
+            self.m_history.append(self.m)
+            self.b_history.append(self.b)
             self.m = self.m - self.learning_rate * 1 / self.datalen * np.sum((self.y - self.prices) * np.array(self.years))
             self.b = self.b - self.learning_rate * 1 / self.datalen * np.sum(self.y - self.prices)
             
@@ -47,11 +58,9 @@ class LinearRegression:
             
             it += 1
 
-        return self.y
+        return self.y, self.j_history, self.m_history, self.b_history
 
-
-    def get_prediction (self,years):
-
+    def get_prediction (self, years):
         y = self.m * years + self.b
         return y
 
@@ -63,7 +72,7 @@ def main():
     plt.ylabel('Price')
 
     # Calcular los valores ajustados
-    fitted_values = lr.fit()
+    fitted_values, j, m , b = lr.fit()
 
     # Graficar los datos y la línea de regresión lineal
     plt.plot(lr.years, fitted_values, color='red')
@@ -71,8 +80,34 @@ def main():
     plt.legend(['Data', 'Linear Regression'])
     plt.show()
 
-
     print(lr.get_prediction(3))
+
+
+    plt.plot(m, j, color='red')
+
+    plt.legend(['m', 'j'])
+    plt.show()
+
+
+    # Crear una cuadrícula de valores para m y b
+    m_vals = np.linspace(min(m)-1000, max(m)+1000, 300)
+    b_vals = np.linspace(min(b)-1000, max(b)+1000, 300)
+    M, B = np.meshgrid(m_vals, b_vals)
+    J = np.zeros_like(M)
+
+    for i in range(M.shape[0]):
+        for j in range(M.shape[1]):
+            J[i, j] = lr.compute_cost(M[i, j], B[i, j])
+
+    # Graficar la superficie del costo
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_surface(M, B, J, cmap='viridis')
+    ax.set_xlabel('m')
+    ax.set_ylabel('b')
+    ax.set_zlabel('J')
+    plt.title('Cost function J(m, b)')
+    plt.show()
 
 if __name__ == '__main__':
     main()
